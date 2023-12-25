13693e8c723b8e50cb525a85f636cd1cb96d3f4a
Yu'sDesktop <1526892925@qq.com>
2023-12-25 22:15:31 +0800

    二叉树

--- 二叉树.cpp
+++ 二叉树.cpp
@@ -8,6 +8,7 @@ typedef struct TreeNode{
 	struct TreeNode *left;
 	struct TreeNode *right;
 }TreeNode;
+int *arr=(int*)malloc(sizeof(int)*MAX_SIZE);
 TreeNode *newTreeNode(int val){
 	TreeNode *node;
 	node=(TreeNode*)malloc(sizeof(TreeNode));
@@ -17,14 +18,31 @@ TreeNode *newTreeNode(int val){
 	node->right=NULL;
 	return node;
 }
+int *preOrder(TreeNode *root,int *size){
+	if(root==NULL) return arr;
+	arr[(*size)++]=root->val;
+	preOrder(root->left,size);
+	preOrder(root->right,size);
+} 
+int *inOrder(TreeNode *root,int *size){
+	if(root==NULL) return arr;
+	inOrder(root->left,size);
+	arr[(*size)++]=root->val;
+	inOrder(root->right,size);
+}
+int *postOrder(TreeNode *root,int *size){
+	if(root==NULL) return arr;
+	postOrder(root->left,size);
+	postOrder(root->right,size);
+	arr[(*size)++]=root->val;
+}
 int *leverOrder(TreeNode *root,int *size){
 	int front=0,rear=0;
-	int index,*arr;
+	int index;
 	TreeNode *node;
 	TreeNode **queue;
 	queue=(TreeNode**)malloc(sizeof(TreeNode*)*MAX_SIZE);
 	queue[rear++]=root;
-	arr=(int*)malloc(sizeof(int)*MAX_SIZE);
 	index=0;
 	while(front<rear){
 		node=queue[front++];
@@ -51,8 +69,9 @@ int main(){
 	n1->right=n3;
 	n2->left=n4;
 	n2->right=n5;
-	int Size=5;
-	int *arr=leverOrder(n1,&Size);
+	int Size=0;
+	//arr=leverOrder(n1,&Size);
+	inOrder(n1,&Size);
 	for(int i=0;i<5;i++)	printf("%d\n",arr[i]);
 	return 0;
 }
