aa3d62792b80ce72f45a4b88ffcb4f1be65bec35
Yu的Deskbook <1526892925@qq.com>
2023-12-29 23:23:03 +0800

    元旦后好累

--- 图.cpp
+++ 图.cpp
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
+#define MAX_SIZE 100
 typedef struct{
 	int vertices[MAX_SIZE];
-	int adjMat[MAX_SIZE][MAX_SIZE;
+	int adjMat[MAX_SIZE][MAX_SIZE];
 	int size;
-}GraphAdMat;
-GraphAdjMat *newGrapgAdjMat(){
+}GraphAdjMat;
+GraphAdjMat *newGraphAdjMat(){
 	GraphAdjMat *graph=(GraphAdjMat*)malloc(sizeof(GraphAdjMat));
 	graph->size=0;
 	for(int i=0;i<MAX_SIZE;i++){
@@ -26,7 +27,9 @@ void addVertex(GraphAdjMat *graph,int val){
 	}
 	int n=graph->size;
 	graph->vertices[n]=val;
-	for(int i=0;i<=n;i++)	graph->adjMat[n][j]=graph->adjMat[i][n]=0;
+	for(int i=0;i<=n;i++){
+		graph->adjMat[n][i]=graph->adjMat[i][n]=0;
+	}
 	graph->size++;
 }
 void removeVertex(GraphAdjMat *graph,int index){
@@ -34,7 +37,7 @@ void removeVertex(GraphAdjMat *graph,int index){
 		printf("顶点索引越界\n");
 		return ;
 	}
-	for(int i=index;i<graph->sizz-1;i++){
+	for(int i=index;i<graph->size-1;i++){
 		graph->vertices[i]=graph->vertices[i+1];
 	}
 	for(int i=index;i<graph->size-1;i++){
@@ -43,9 +46,48 @@ void removeVertex(GraphAdjMat *graph,int index){
 		}
 	}
 	for(int i=0;i<graph->size;i++){
-		for(int j=index)
+		for(int j=index;j<graph->size-1;j++){
+			graph->adjMat[i][j]=graph->adjMat[i][j+1]; 
+		}
 	}
+	graph->size--;
 }
-int main(){
+void addEdge(GraphAdjMat *graph,int i,int j){
+	if(i<0||j<0||i>=graph->size||j>=graph->size||i==j){
+		printf("数组边界越界\n");
+		return;
+	}
+	graph->adjMat[i][j]=1;
+	graph->adjMat[j][i]=1;
+}
+void removeEdge(GraphAdjMat *graph,int i,int j){
+	if(i<0||j<0||i>=graph->size||j>=graph->size||i==j){
+		printf("数组边界越界\n");
+		return;
+	}
+	graph->adjMat[i][j]=0;
+	graph->adjMat[j][i]=0;
+}
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]); // 数组名arr被当作指针使用
+    }
+}
+void printgraphAdjMat(GraphAdjMat *graph){
+	printf("顶点列表 = ");
+	printArray(graph->vertices,graph->size);
+	printf("邻接矩阵 =\n");
+	for(int i=0;i<graph->size;i++){
+		printArray(graph->adjMat[i],graph->size);
+	} 
+} 
+int main(){  
+	GraphAdjMat *graph=newGraphAdjMat();
+	addVertex(graph,1);
+	addVertex(graph,2);
+	addVertex(graph,3);
+	addVertex(graph,4);
+	addVertex(graph,5);
+	printgraphAdjMat(graph);
 	return 0; 
 }
